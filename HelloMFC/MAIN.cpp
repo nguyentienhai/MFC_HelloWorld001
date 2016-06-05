7ee0b134567bd4f590e0246eb4e831a550de707e
Nguyễn Tiến Hải <Haint88@gmail.com>
2016-06-05 22:07:39 +0700

    adjust comment

--- HelloMFC/MAIN.cpp
+++ HelloMFC/MAIN.cpp
@@ -1,23 +1,23 @@
 //01/16/2010 C. Germany Visual Studio 2008 MFC Template
 /*
-     Notes: This is a MFC template to create graphical C++ apps without
-	        using the Visual Studio 2008 wizards. It is stripped down to
+	 Notes: This is a MFC template to create graphical C++ apps without
+			using the Visual Studio 2008 wizards. It is stripped down to
 			a bare minimum of code so apps are as simplified as possible.
 			You must disable some automated features and manually set a
 			few properties for projects using this template.
 			1. Create an EMPTY Win32 project. Click -> "File" -> -> "New"
 			   -> "Project" -> "Win32" -> "Win32 Project".
-		    2. Name is and select a directory.
+			2. Name is and select a directory.
 			3. Select "Application Settings" -> "Empty Project" then click "Finish".
 			4. Rt-click project, select "Properties" -> "Configuration Properties"
 			   -> "General" -> "Use of MFC" and change it to 
 			   "Use MFC in a Static Library". This will give you MFC components.
-		    5. Disable Incremental Linking. Rt-click project, select "Properties" 
+			5. Disable Incremental Linking. Rt-click project, select "Properties" 
 			   -> "Configuration Properties" -> "Linker" -> "General" ->
 			   "Enable Incremental Linking" and set it to "NO".
-                    6. Add a resources file. Rt-click resources and add a DIALOG object.
-                    7. Change the enumerated constant to match the Dialog name.
-		    8. Note that unlike 2003, when calling SetWindowText() in 2008 the
+			6. Add a resources file. Rt-click resources and add a DIALOG object.
+			7. Change the enumerated constant to match the Dialog name.
+			8. Note that unlike 2003, when calling SetWindowText() in 2008 the
 			   string passed in must be cast/converted using "L".	   
 	Reference: http://www.networkingprogramming.com/1024x768/2008_Bare_Template.html
 */
